3766aff466db339925d42da15abc68913fccbee2
Djordje Ungar <djordje.ungar@gmail.com>
2020-07-15 17:43:52 +0200

    Mandala Maker v0.1.0

---
+++ README.md
@@ -0,0 +1,23 @@
+# HTML5 Mandala Maker
+[![GitHub license](https://img.shields.io/github/license/ArtBIT/html5-mandala.svg)](https://github.com/ArtBIT/html5-mandala) [![GitHub stars](https://img.shields.io/github/stars/ArtBIT/html5-mandala.svg)](https://github.com/ArtBIT/html5-mandala)  [![awesomeness](https://img.shields.io/badge/awesomeness-maximum-red.svg)](https://github.com/ArtBIT/html5-mandala)
+
+This is a tiny web app that generates pixel sprite sheets based on a template.
+
+| | | | |
+|:---:|:---:|:---:|:---:|
+| <img src="./assets/pirot.jpg"> | <img src="./assets/pirot-cilim.jpg"> | <img src="./assets/grandmatiles.jpg"> | <img src="./assets/greatgrandmatiles.jpg"> |
+
+# Demo
+Try out the live demo http://artbit.github.io/html5-mandala/
+
+# Running it locally
+```
+git clone https://github.com/ArtBIT/html5-mandala.git
+cd html5-mandala
+npm run dev
+open public/index.html
+```
+
+# License
+
+[MIT](LICENSE.md)
---
+++ src/gui.ts
@@ -0,0 +1,111 @@
+import gui from "dat.gui";
+
+class Gui {
+    constructor(params) {
+        this.listeners = {};
+        this.config = params;
+        this.gui = new gui.GUI();
+
+        const handle = name => value => {
+            this.triggerChange(name, value);
+        };
+
+        const guiCanvas = this.gui.addFolder("Canvas");
+        guiCanvas
+            .add(params, "width")
+            .min(10)
+            .max(4096)
+            .step(1)
+            .onChange(handle("width"));
+        guiCanvas
+            .add(params, "height")
+            .min(10)
+            .max(4096)
+            .step(1)
+            .onChange(handle("height"));
+        guiCanvas
+            .add(params, "scale")
+            .min(1)
+            .max(4)
+            .step(1)
+            .onChange(handle("scale"));
+        guiCanvas
+            .add(params, "angle")
+            .min(0)
+            .max(360)
+            .onChange(handle("angle"));
+        guiCanvas.open();
+
+        const guiPattern = this.gui.addFolder("Pattern");
+        guiPattern
+            .add(params, "symmetries")
+            .min(4)
+            .max(32)
+            .step(1)
+            .onChange(handle("symmetries"));
+        guiPattern
+            .add(params, "patternScale")
+            .min(0.05)
+            .max(4)
+            .step(0.05)
+            .onChange(handle("patternScale"));
+        guiPattern
+            .add(params, "patternAngle")
+            .min(0)
+            .max(360)
+            .onChange(handle("patternAngle"));
+
+        guiPattern.addColor(params, "offset").onChange(handle("offset"));
+        guiPattern.open();
+    }
+    addEventListener(eventName, eventCallback) {
+        this.listeners[eventName] = this.listeners[eventName] || [];
+        this.listeners[eventName].push(eventCallback);
+    }
+    removeEventListener(eventName, eventCallback) {
+        this.listeners[eventName] = this.listeners[eventName] || [];
+        this.listeners[eventName] = this.listeners[eventName].filter(
+            callback => callback !== eventCallback
+        );
+    }
+    triggerChange(name, value) {
+        const type = "change";
+        this.listeners[type] &&
+            this.listeners[type].forEach(callback =>
+                callback({ type, name, value })
+            );
+    }
+    getValue(name) {
+        return this.config[name];
+    }
+    setValue(name, value) {
+        const controller = this._findControllerFor(name);
+        if (controller) {
+            controller.setValue(value);
+        }
+    }
+    increaseValue(name, value) {
+        this.setValue(name, this.getValue(name) + value);
+    }
+    _findControllerFor(name) {
+        let controller = this.gui.__controllers.find(function(elem) {
+            return elem.property === name;
+        });
+        if (!controller) {
+            controller = Object.keys(this.gui.__folders).reduce(
+                (result, folderName) => {
+                    if (!result) {
+                        result = this.gui.__folders[
+                            folderName
+                        ].__controllers.find(elem => elem.property === name);
+                    }
+                    return result;
+                },
+                undefined
+            );
+        }
+        return controller;
+    }
+}
+
+export default Gui;
